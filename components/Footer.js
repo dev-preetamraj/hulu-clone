32676c1329fe329d5ec112e5fcaa50a58c42141f
Preetam Raj <iitianpreetam@gmail.com>
2022-06-03 18:05:59 +0530

    ic

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <footer className="bottom-0 h-10 flex items-center justify-center bg-black">
+            <p>Made with ❤ by Preetam</p>
+        </footer>
+    );
+};
+
+export default Footer;
